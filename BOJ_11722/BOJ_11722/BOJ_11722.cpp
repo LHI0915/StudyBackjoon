c46a5289220b2bbe7a6b576d4e7bf4d27e017fad
LHI0915 <gowns1998@gmail.com>
2019-01-11 16:25:28 +0900

    BOJ_11722

---
+++ BOJ_11722/BOJ_11722/BOJ_11722.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+	int ans=0,testcase,arr[1001],count=1,dp[1001],check=0;
+	scanf("%d",&testcase);
+	for(int i=0;i<testcase;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<testcase;i++){
+		dp[i]=1;
+		for(int j=0;j<i;j++){
+			if(arr[i]<arr[j]){
+				dp[i]=max(dp[i],dp[j]+1);
+			}
+		}
+		ans=max(ans,dp[i]);
+	}
+	printf("%d\n",ans);
+}
